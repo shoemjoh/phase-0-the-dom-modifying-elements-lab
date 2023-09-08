653b38cb6b40d6508df1d6db7a992d0b0cf513e0
John <shoemjoh@gmail.com>
2023-09-08 23:26:34 +0200

    dom lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// Remove element by ID
+const element = document.getElementById("main");
+element.remove();
+
+// Add newHeader variable that points to H1 node;
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "John is the champion"
